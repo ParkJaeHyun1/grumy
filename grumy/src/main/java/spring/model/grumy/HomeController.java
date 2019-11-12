9c6a8cc4d1c956dac480097b00b6d99f2787ed85
parkjaehyun <wogus519@naver.com>
2019-11-12 13:08:31 +0900

    footer header

--- grumy/src/main/java/spring/model/grumy/HomeController.java
+++ grumy/src/main/java/spring/model/grumy/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		System.out.println("하이헬로우안녕");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "main";
+		return "/home";
 	}
 	
 }
