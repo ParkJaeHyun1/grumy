62a6dc0d098a0255fe71a64ab3a5234ceb0960eb
parkjaehyun <wogus519@naver.com>
2019-11-15 17:38:34 +0900

    ㅇㄴㅁㄻㄴㅇㄹ

--- grumy/src/main/java/spring/model/cart/CartDTO.java
+++ grumy/src/main/java/spring/model/cart/CartDTO.java
@@ -5,39 +5,122 @@ public class CartDTO {
 	private String id;
 	private int itemNo;
 	private int count;
+	private String itemPicture;
+	private String itemTitle;
+	private int itemPrice;
+	private String itemSize;
+	private String itemColor;
+	
 	
 	public CartDTO() {
 		// TODO Auto-generated constructor stub
 	}
-	public CartDTO(int no, String id, int itemNo, int count) {
+
+
+	public CartDTO(int no, String id, int itemNo, int count, String itemPicture, String itemTitle, int itemPrice,
+			String itemSize, String itemColor) {
 		super();
 		this.no = no;
 		this.id = id;
 		this.itemNo = itemNo;
 		this.count = count;
+		this.itemPicture = itemPicture;
+		this.itemTitle = itemTitle;
+		this.itemPrice = itemPrice;
+		this.itemSize = itemSize;
+		this.itemColor = itemColor;
 	}
+
+
 	public int getNo() {
 		return no;
 	}
+
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
+
 	public String getId() {
 		return id;
 	}
+
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
+
 	public int getItemNo() {
 		return itemNo;
 	}
+
+
 	public void setItemNo(int itemNo) {
 		this.itemNo = itemNo;
 	}
+
+
 	public int getCount() {
 		return count;
 	}
+
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
+
+	public String getItemPicture() {
+		return itemPicture;
+	}
+
+
+	public void setItemPicture(String itemPicture) {
+		this.itemPicture = itemPicture;
+	}
+
+
+	public String getItemTitle() {
+		return itemTitle;
+	}
+
+
+	public void setItemTitle(String itemTitle) {
+		this.itemTitle = itemTitle;
+	}
+
+
+	public int getItemPrice() {
+		return itemPrice;
+	}
+
+
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+
+	public String getItemSize() {
+		return itemSize;
+	}
+
+
+	public void setItemSize(String itemSize) {
+		this.itemSize = itemSize;
+	}
+
+
+	public String getItemColor() {
+		return itemColor;
+	}
+
+
+	public void setItemColor(String itemColor) {
+		this.itemColor = itemColor;
+	}
+
+	
+
 }
--- grumy/src/main/java/spring/model/grumy/CartController.java
+++ grumy/src/main/java/spring/model/grumy/CartController.java
@@ -1,11 +1,16 @@
 package spring.model.grumy;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import spring.model.cart.CartDTO;
+import spring.model.item.ItemDTO;
 import spring.model.mapper.CartMapper;
 
 
@@ -15,8 +20,11 @@ public class CartController {
 	private CartMapper mapper;
 	
 	@RequestMapping("/cart/list")
-	public String list(HttpServletRequest request) {
-		
+	public String list(HttpServletRequest request, HttpSession session) {
+		//String userID = (String)session.getAttribute("id");
+		String userID = "tester"; 
+		ArrayList<CartDTO> list = mapper.list(userID);
+		request.setAttribute("list",list);
 		return "/cart/list";
 	}
 }
