2692152f80c497bcded5cf93bd97ada303a4ee89
snowa <snowa@DESKTOP-7MJM0QH>
2019-12-13 11:07:12 +0900

    ㅋㅁ

--- grumy/src/main/java/spring/model/grumy/MemberController.java
+++ grumy/src/main/java/spring/model/grumy/MemberController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.mail.internet.MimeMessage;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -30,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
 import spring.model.mapper.MemberMapper;
+import spring.model.member.Coolsms;
 import spring.model.member.MemberDTO;
 import spring.model.member.NaverLoginBO;
 import spring.model.utility.Utility;
@@ -371,5 +371,46 @@ public class MemberController {
 
 		return "/login";
 	}
+	
+	@RequestMapping(value="/member/sendsms")
+	public String sendsms(HttpServletRequest request)throws Exception {
+		 String api_key = "NCS6BLVQHLCKDCZG"; 
+         String api_secret = "PAMLNBACSRKGWC862RET7RSLX9UGUKHA";  
 
+         Coolsms coolsms = new Coolsms(api_key, api_secret);
+         
+         
+         HashMap<String, String> set = new HashMap<String, String>();
+         set.put("to", "01057165993"); 
+
+         set.put("phone", (String)request.getParameter("phone")); // 발신번호, jsp에서 전송한 발신번호를 받아 map에 저장한다.
+         set.put("passwd", (String)request.getParameter("passwd")); // 문자내용, jsp에서 전송한 문자내용을 받아 map에 저장한다.
+         set.put("type", "sms"); // 문자 타입
+
+         System.out.println(set);
+
+         JSONObject result = coolsms.send(set); // 보내기&전송결과받기
+
+         if ((boolean)result.get("status") == true) {
+
+           // 메시지 보내기 성공 및 전송결과 출력
+           System.out.println("성공");
+           System.out.println(result.get("group_id")); // 그룹아이디
+           System.out.println(result.get("result_code")); // 결과코드
+           System.out.println(result.get("result_message")); // 결과 메시지
+           System.out.println(result.get("success_count")); // 메시지아이디
+           System.out.println(result.get("error_count")); // 여러개 보낼시 오류난 메시지 수
+         } else {
+
+           // 메시지 보내기 실패
+           System.out.println("실패");
+           System.out.println(result.get("code")); // REST API 에러코드
+           System.out.println(result.get("message")); // 에러메시지
+         }
+
+         return "/login"; //문자 메시지 발송 성공했을때 number페이지로 이동함
+       }
+
+	
+	
 }
